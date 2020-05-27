168141f5e5144bea23e654458dc8ecc6224cfd9d
Cheng <54488402@qq.com>
2020-05-27 18:07:13 +0800

    2020.05.27

---
+++ .history/src/views/note/edit/Webpack_20200527112226.vue
@@ -0,0 +1,18 @@
+<!-- @format -->
+
+<template>
+  <div class="Webpack" :class="item.jsj === 0 ? 'red' : 'black'">
+    Webpack
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Webpack",
+})
+export default class ClassName extends Vue {}
+</script>
+
+<style lang="less" scope></style>
---
+++ .history/src/views/note/edit/Webpack_20200527112408.vue
@@ -0,0 +1,22 @@
+<!-- @format -->
+
+<template>
+  <div class="Webpack" :style="item.jsj === 0 ? 'red' : 'black'">
+    Webpack
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Webpack",
+})
+export default class ClassName extends Vue {
+  private item = {
+    jsj: 0,
+  };
+}
+</script>
+
+<style lang="less" scope></style>
